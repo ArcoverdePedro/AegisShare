4973613b2f1c2830ea9629b3c17277eb22e5ebe0
Pedro Abilio Pereira Arcoverde <103291108+ArcoverdePedro@users.noreply.github.com>
2026-01-09 14:03:49 -0300

    Refactor granian command in commands.sh
    Updated granian command to use environment variable for port and adjusted worker and thread counts.

--- commands.sh
+++ commands.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 uv run manage.py shell -c 'from django.core.cache import cache; cache.clear()' --verbosity 0
 
@@ -6,4 +7,9 @@ uv run manage.py migrate
 
 uv run manage.py collectstatic --noinput --clear
 
-uv run granian mysite.asgi:application --host 0.0.0.0 --port 8000 --interface asgi --workers 5 --runtime-threads 2
+exec uv run granian mysite.asgi:application \
+  --host 0.0.0.0 \
+  --port $PORT \
+  --interface asgi \
+  --workers 1 \
+  --runtime-threads 1
