4b40cfa3c25a3fe1e87d3c89c3a8bc9648a4f946
Pedro Abilio Pereira Arcoverde <103291108+ArcoverdePedro@users.noreply.github.com>
2026-01-09 14:06:57 -0300

    Remove worker and thread options from command

--- commands.sh
+++ commands.sh
@@ -10,6 +10,4 @@ uv run manage.py collectstatic --noinput --clear
 exec uv run granian mysite.asgi:application \
   --host 0.0.0.0 \
   --port $PORT \
-  --interface asgi \
-  --workers 1 \
-  --runtime-threads 1
+  --interface asgi
