fa023b02c76d8c7ae2e03361a8facd64e3eda1df
Pedro Abilio Pereira Arcoverde <103291108+ArcoverdePedro@users.noreply.github.com>
2026-09-03 17:17:14 -0300

    refactor: explicita camada de compatibilidade de views

--- aegis_share/views.py
+++ aegis_share/views.py
@@ -1,18 +1,44 @@
-"""Compatibilidade para imports antigos.
+"""Compatibilidade temporaria para imports antigos.
 
 Novas rotas importam controllers de ``aegis_share.web`` diretamente.
 Este modulo pode ser removido em uma futura major version.
 """
 
-from .web.auth import FirstSuperuserCreateView, cadastro, custom_login, login_2fa, user_profile
-from .web.chat import chat_index, get_or_create_conversation, load_conversation, user_list
-from .web.dashboard import home, sobre
-from .web.files import (
-    arquivos,
-    buscar_arquivo,
-    buscar_cliente,
-    buscar_funcionario,
-    upload,
-)
+from .web.auth import FirstSuperuserCreateView as FirstSuperuserCreateView
+from .web.auth import cadastro as cadastro
+from .web.auth import custom_login as custom_login
+from .web.auth import login_2fa as login_2fa
+from .web.auth import user_profile as user_profile
+from .web.chat import chat_index as chat_index
+from .web.chat import get_or_create_conversation as get_or_create_conversation
+from .web.chat import load_conversation as load_conversation
+from .web.chat import user_list as user_list
+from .web.dashboard import home as home
+from .web.dashboard import sobre as sobre
+from .web.files import arquivos as arquivos
+from .web.files import buscar_arquivo as buscar_arquivo
+from .web.files import buscar_cliente as buscar_cliente
+from .web.files import buscar_funcionario as buscar_funcionario
+from .web.files import upload as upload
 
 user = user_profile
+
+__all__ = [
+    "FirstSuperuserCreateView",
+    "arquivos",
+    "buscar_arquivo",
+    "buscar_cliente",
+    "buscar_funcionario",
+    "cadastro",
+    "chat_index",
+    "custom_login",
+    "get_or_create_conversation",
+    "home",
+    "load_conversation",
+    "login_2fa",
+    "sobre",
+    "upload",
+    "user",
+    "user_list",
+    "user_profile",
+]
