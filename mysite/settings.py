34842934708dc58f0532d89d594af17631ed9ba5
Pedro Abilio Pereira Arcoverde <103291108+ArcoverdePedro@users.noreply.github.com>
2026-09-03 17:37:50 -0300

    fix: configura DATABASE_URL com django-environ 0.14

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,9 @@ ASGI_APPLICATION = "mysite.asgi.application"
 
 DATABASE_URL = env.str("DATABASE_URL", default="")
 if DATABASE_URL:
-    DATABASES = {"default": env.db_url("DATABASE_URL", conn_max_age=60)}
+    DATABASES = {
+        "default": env.db_url_config(DATABASE_URL, conn_max_age=60)
+    }
 elif not DEBUG:
     raise ImproperlyConfigured(
         "DATABASE_URL e obrigatoria em producao. Use PostgreSQL interno ou um host externo."
