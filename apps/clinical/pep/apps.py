b25d27d68b5a97abab30f7b79c03e9e84e72890f
Pedro Abilio Pereira Arcoverde <103291108+ArcoverdePedro@users.noreply.github.com>
2026-09-11 12:53:40 -0300

    feat(pep): add app config

---
+++ apps/clinical/pep/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PepConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.clinical.pep"
+    verbose_name = "PEP — Prontuário Eletrônico"
