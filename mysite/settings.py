6ad68cb4f5fb33c444414af573cb409c9d54cf39
Pedro Abilio Pereira Arcoverde <103291108+ArcoverdePedro@users.noreply.github.com>
2026-09-14 13:05:20 -0300

    fix(rx): make settings wrapper lint-clean

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,7 @@
 from .settings_base import *  # noqa: F401,F403
+from .settings_base import INSTALLED_APPS as BASE_INSTALLED_APPS
 
-INSTALLED_APPS = [  # noqa: F405
-    *INSTALLED_APPS,
+INSTALLED_APPS = [
+    *BASE_INSTALLED_APPS,
     "apps.clinical.prescription.apps.PrescriptionConfig",
 ]
