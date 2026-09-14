76fbfda2921a11ded437c244e8f1bb38e059552b
Pedro Abilio Pereira Arcoverde <103291108+ArcoverdePedro@users.noreply.github.com>
2026-09-14 10:43:14 -0300

    feat(adt): expose lifecycle history as read-only admin

--- apps/clinical/adt/admin.py
+++ apps/clinical/adt/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Admission, Bed, BedOccupancy, Location, UserLocationAccess
+from .models import (
+    Admission,
+    Bed,
+    BedOccupancy,
+    Discharge,
+    Location,
+    Transfer,
+    UserLocationAccess,
+)
 
 
 @admin.register(Location)
@@ -34,3 +42,44 @@ class AdmissionAdmin(admin.ModelAdmin):
 class BedOccupancyAdmin(admin.ModelAdmin):
     list_display = ("id", "bed", "admission", "started_at", "ended_at")
     list_filter = ("end_reason", "bed__location")
+
+
+class ImmutableLifecycleAdmin(admin.ModelAdmin):
+    """Permite inspeção administrativa sem editar ou apagar histórico concluído."""
+
+    def get_readonly_fields(self, request, obj=None):
+        return tuple(field.name for field in self.model._meta.fields)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Transfer)
+class TransferAdmin(ImmutableLifecycleAdmin):
+    list_display = (
+        "id",
+        "admission",
+        "source_occupancy",
+        "destination_occupancy",
+        "transferred_at",
+        "transferred_by",
+    )
+    list_filter = ("transferred_at",)
+
+
+@admin.register(Discharge)
+class DischargeAdmin(ImmutableLifecycleAdmin):
+    list_display = (
+        "id",
+        "admission",
+        "discharged_at",
+        "disposition",
+        "discharged_by",
+    )
+    list_filter = ("disposition", "discharged_at")
