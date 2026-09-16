727c5161787145d4855ce1375a0ed0578738f2ce
Pedro Abilio Pereira Arcoverde <103291108+ArcoverdePedro@users.noreply.github.com>
2026-09-16 08:26:28 -0300

    test(ui): cover permission-aware clinical navigation

---
+++ aegis_share/tests/test_clinical_navigation.py
@@ -0,0 +1,70 @@
+from django.contrib.auth.models import Permission
+from django.test import TestCase
+from django.urls import reverse
+
+from aegis_share.models import CustomUser
+
+
+class ClinicalNavbarTests(TestCase):
+    def _make_user(self, username, role):
+        return CustomUser.objects.create_user(
+            username=username,
+            password="test-password",
+            nivel_permissao=role,
+        )
+
+    def _grant(self, user, app_label, codename):
+        permission = Permission.objects.get(
+            content_type__app_label=app_label,
+            codename=codename,
+        )
+        user.user_permissions.add(permission)
+
+    def _assert_clinical_link(self, response, url_name, present):
+        link = f'href="{reverse(url_name)}"'
+        if present:
+            self.assertContains(response, link)
+        else:
+            self.assertNotContains(response, link)
+
+    def test_employee_without_capabilities_hides_specialized_clinical_links(self):
+        employee = self._make_user("nav-employee-basic", "FUNC")
+        self.client.force_login(employee)
+
+        response = self.client.get(reverse("home"))
+
+        self.assertEqual(response.status_code, 200)
+        self._assert_clinical_link(response, "pep:patient_list", True)
+        self._assert_clinical_link(response, "adt:bed_map", False)
+        self._assert_clinical_link(response, "prescription:prescription_list", False)
+        self._assert_clinical_link(response, "nursing:worklist", False)
+
+    def test_employee_sees_only_explicitly_granted_clinical_modules(self):
+        employee = self._make_user("nav-employee-clinical", "FUNC")
+        self._grant(employee, "adt", "view_bed_map")
+        self._grant(employee, "prescription", "view_medication_request")
+        self._grant(employee, "nursing", "view_nursing")
+        self.client.force_login(employee)
+
+        response = self.client.get(reverse("home"))
+
+        self.assertEqual(response.status_code, 200)
+        self._assert_clinical_link(response, "pep:patient_list", True)
+        self._assert_clinical_link(response, "adt:bed_map", True)
+        self._assert_clinical_link(response, "prescription:prescription_list", True)
+        self._assert_clinical_link(response, "nursing:worklist", True)
+
+    def test_client_with_misassigned_capabilities_gets_no_clinical_navigation(self):
+        client_user = self._make_user("nav-client", "CLI")
+        self._grant(client_user, "adt", "view_bed_map")
+        self._grant(client_user, "prescription", "view_medication_request")
+        self._grant(client_user, "nursing", "view_nursing")
+        self.client.force_login(client_user)
+
+        response = self.client.get(reverse("home"))
+
+        self.assertEqual(response.status_code, 200)
+        self._assert_clinical_link(response, "pep:patient_list", False)
+        self._assert_clinical_link(response, "adt:bed_map", False)
+        self._assert_clinical_link(response, "prescription:prescription_list", False)
+        self._assert_clinical_link(response, "nursing:worklist", False)
